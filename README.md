99e077b36ff175b6425e051764c6bc810862f59a
Hasnain Dehwar <hasnaindehwar78@gmail.com>
2026-01-07 13:15:46 +0500

    Added student management system and updated files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript_Practice
+# JavaScript Practice
 
-
-ProblemsFaced in Todoapp while getting data from localstroage and showing the stored data on the screen.
+This repository contains my JavaScript practice projects.
+I am learning core JavaScript concepts and building small projects.
--- live_Se_filter/data.js
+++ live_Se_filter/data.js
@@ -115,7 +115,7 @@ const users = [
   },
   {
     name: "Shahzaib Khan",
-    image: " https://images.unsplash.com/photo-1611316185995-9624c94487d1?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+    image: "https://images.unsplash.com/photo-1611316185995-9624c94487d1?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
     bio: "Tech learner exploring full-stack development.",
     followers: 810
   },
