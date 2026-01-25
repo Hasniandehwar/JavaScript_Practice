daeadbb56967d1a4d744014462dd27cb87d20d36
Hasnain Dehwar <hasnaindehwar78@gmail.com>
2026-01-25 19:32:04 +0500

    code Added

---
+++ Expense_Tracker/main.js
@@ -0,0 +1,21 @@
+const menuicon=document.querySelector(".bi");
+const side_bar=document.querySelector(".side_bar")
+const div=document.querySelector(".div");
+const column=document.querySelector("#cloumn")
+const btns=document.querySelector("#btns")
+menuicon.addEventListener("click" , ()=>{
+    
+    if (menuicon.classList.contains("bi-list")){
+        side_bar.className="d-flex  d-md-block d-lg-block col-lg-2 col-md-3 side_bar ";
+        div.className="col-md-7 col-lg-9 div ms-md-3 ms-p-3 mt-3 border rounded-3"
+        menuicon.className="bi bi-x d-lg-none";
+        column.className="flex-row";
+    } else{
+        side_bar.className = "d-none  d-md-block d-lg-block col-lg-2 col-md-3 side_bar";
+        div.className = "col-md-7 col-lg-9 div ms-md-3 ms-p-3 mt-3 border rounded-3";
+        menuicon.className="d-lg-none d-md-none bi bi-list";
+  }
+    
+})
+
+
