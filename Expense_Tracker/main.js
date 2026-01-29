08fa3085a976b9bda5b487ed3de637b273a7f0b1
Hasnain Dehwar <hasnaindehwar78@gmail.com>
2026-01-29 12:00:04 +0500

    Budget adding page completed

--- Expense_Tracker/main.js
+++ Expense_Tracker/main.js
@@ -1,4 +1,4 @@
-const menuicon=document.querySelector(".bi");
+const menuicon=document.querySelector("#Menuicon");
 const side_bar=document.querySelector(".side_bar")
 const div=document.querySelector(".div");
 const column=document.querySelector("#cloumn")
@@ -6,7 +6,7 @@ const btns=document.querySelector("#btns")
 menuicon.addEventListener("click" , ()=>{
     
     if (menuicon.classList.contains("bi-list")){
-        side_bar.className="d-flex  d-md-block d-lg-block col-lg-2 col-md-3 side_bar ";
+        side_bar.className="d-flex  d-md-block d-lg-block col-lg-2 col-md-3 side_bar";
         div.className="col-md-7 col-lg-9 div ms-md-3 ms-p-3 mt-3 border rounded-3"
         menuicon.className="bi bi-x d-lg-none";
         column.className="flex-row";
@@ -40,14 +40,50 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const value = Number(BudgetInput.value);
     if ((value >= 1000)) {
+        const modu=document.querySelector(".popup")
         Budgetamount += value;
         console.log(Budgetamount);
-        alert("Budget Amount Added")
+        setTimeout(()=>{
+            setTimeout(()=>{
+                Form_container.classList.add("d-none")
+            } , 100)
+            
+            modu.classList.remove("d-none");  
+        }, 500)
+        // page return to Dash
     }else{
         small2.textContent = "Amount must be at least 1000";
         small2.className="d-flex text-white";
         setTimeout(()=>{
             small2.className="d-none";
         } , 3000);
-    } 
+    }
+
 });
+
+// Add_Budget BTN
+const Add_Budget_btn=document.querySelector("#Add_Budget");
+const Main_div_=document.querySelector("#Main_div");
+const Form_container=document.querySelector("#Form_container");
+const popcontainer=document.querySelector(".popcontainer")
+Add_Budget_btn.addEventListener("click" , ()=>{
+    Add_Budget();
+});
+
+function Add_Budget(){
+    Form_container.classList.remove("d-none");
+    Main_div_.classList.toggle("d-none");
+}
+
+function returnHomepage(){
+    setTimeout(()=>{
+        Main_div_.classList.remove("d-none");
+        popcontainer.classList.add("d-none");
+    }, 500);
+}
+
+// ok btn in mod
+const ok_btn_popup=document.querySelector(".ok");
+ok_btn_popup.addEventListener("click" , ()=>{
+    returnHomepage();
+})
