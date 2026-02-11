52178b5cbb6257ada9770c3baab9f0091b382ebb
Hasnain Dehwar <hasnaindehwar78@gmail.com>
2026-02-11 20:26:06 +0500

    code Added

--- store-management-system/scripts/modules/stock.js
+++ store-management-system/scripts/modules/stock.js
@@ -1,1 +1,23 @@
 // # stock update logic
+import { dataBase_ls } from "../core/storage.js";
+
+class Stocks{
+    constructor(){
+        // pass
+    }
+
+    getStock(){
+        let data=dataBase_ls.getData("products");
+        return data 
+    }
+
+    updateStock(pro_name){
+        let data=this.getStock();
+        const updatedStock= data.filter(function(Product){
+            if(Product.Product_name===pro_name)
+        });
+    }
+}
+
+let p1=new Stocks();
+p1.updateStock("Apple")
